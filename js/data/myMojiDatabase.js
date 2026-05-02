b291b9cf5bb4f539f219d49cf2a68b56e3e6d040
mick-lockwood <mick.lockwood87@gmail.com>
2026-05-02 17:21:50 +0930

    Create myMojiDatabase.js

---
+++ js/data/myMojiDatabase.js
@@ -0,0 +1,21 @@
+const myMojiDatabase = [
+    // --- COMMONS ---
+    { id: "m_001", name: "Classic Smile", rarity: "Common", baseValue: 1.50 },
+    { id: "m_002", name: "Sweat Drop", rarity: "Common", baseValue: 1.25 },
+    { id: "m_003", name: "Thumbs Up", rarity: "Common", baseValue: 1.00 },
+    { id: "m_004", name: "Sleepy Zzz", rarity: "Common", baseValue: 1.50 },
+    { id: "m_005", name: "Slight Frown", rarity: "Common", baseValue: 0.50 },
+
+    // --- RARES ---
+    { id: "m_006", name: "Star Eyes", rarity: "Rare", baseValue: 15.00 },
+    { id: "m_007", name: "Cool Glasses", rarity: "Rare", baseValue: 18.50 },
+    { id: "m_008", name: "Party Horn", rarity: "Rare", baseValue: 12.00 },
+
+    // --- EPICS ---
+    { id: "m_009", name: "Melting Face", rarity: "Epic", baseValue: 85.00 },
+    { id: "m_010", name: "Mind Blown", rarity: "Epic", baseValue: 110.00 },
+
+    // --- LEGENDARIES ---
+    { id: "m_011", name: "Golden Crown", rarity: "Legendary", baseValue: 500.00 },
+    { id: "m_012", name: "Holographic Ghost", rarity: "Legendary", baseValue: 1200.00 }
+];
