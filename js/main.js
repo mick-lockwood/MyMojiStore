d121939e1d51a7f957e42b1b3fec8fc7d681cddb
mick-lockwood <mick.lockwood87@gmail.com>
2026-05-23 13:34:46 +0930

    Update main.js

--- js/main.js
+++ js/main.js
@@ -479,7 +479,7 @@ function createCardGraphic(scene, mojiData) {
     
     // NUMBER (#001): Currently at X: 95, Y: 135 (Bottom right corner)
     let numStr = '#' + mojiData.id.split('_')[1];
-    const numTxt = scene.add.text(85, 140, numStr, { 
+    const numTxt = scene.add.text(85, 143.75, numStr, { 
         fontFamily: 'Arial', fontSize: '16px', color: textColor, fontStyle: 'bold' 
     }).setOrigin(1, 0.5);
 
